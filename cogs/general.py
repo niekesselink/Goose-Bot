d72da30bd7d4bace3bdb25ac6211138e5c8dd1d3
Niek Esselink <niek@posteo.net>
2020-04-16 04:36:00 +0200

    A test to see if it works the git pull command..

--- cogs/general.py
+++ cogs/general.py
@@ -13,7 +13,7 @@ class General(commands.Cog):
         """ Shows information about the Goose bot"""
 
         # Just send a message
-        await ctx.send('Honk honk. *Niek Esselink* made me. \n\n**Source:** https://github.com/niekesselink/goose-bot'
+        await ctx.send('Honk honk, *Niek Esselink* made me. \n\n**Source:** https://github.com/niekesselink/goose-bot'
                        + '\n**Version:** ' + subprocess.check_output(["git", "describe", "--always"]).strip().decode("utf-8")
                        + ' **Date:** ' + subprocess.check_output(["git", "log", "-1", "--format=%cd "]).strip().decode("utf-8"))
 
