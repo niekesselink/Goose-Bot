9cf5679dc23c8ae9637411c3f4311a57dbc4974f
Niek Esselink <niek@posteo.net>
2020-04-24 04:17:32 +0200

    Implement the viewing of the playlist

--- cogs/music.py
+++ cogs/music.py
@@ -18,7 +18,7 @@ class Music(commands.Cog):
 
         # Make sure the person using the command is in a voice channel
         if ctx.message.author.voice is None:
-            await ctx.send('Honk honk. Get first in a channel yourself!')
+            await ctx.send(f'**Honk honk.** Get first in a channel yourself {ctx.message.author.mention}!')
             return
 
         # Now go to the channel nicely
@@ -35,44 +35,79 @@ class Music(commands.Cog):
 
         # This command only works when the bot is in a voice channel...
         if ctx.voice_client is None:
-            await ctx.send('Honk honk. I\'m not in a channel!')
+            await ctx.send('**Honk honk.** I\'m not in a channel!')
             return
 
         # Now leave.
         await ctx.voice_client.disconnect()
 
+        # Destroy the queue if we had one...
+        if ctx.guild.id in QUEUES:
+            del(QUEUES[ctx.guild.id])
+
     @commands.command()
     @commands.guild_only()
     async def volume(self, ctx, volume: int):
-        """Changes the volume output of the bot"""
+        """ Changes the volume output of the bot """
 
         # This command only works when the bot is in a voice channel...
         if ctx.voice_client is None:
-            await ctx.send('Honk honk. I\'m not in a channel!')
+            await ctx.send('**Honk honk.** I\'m not in a channel!')
             return
 
         # Ensure we have a source.
         if ctx.voice_client.source is None:
-            await ctx.send('Honk honk. I\'m not playing or have played something before!')
+            await ctx.send(f'**Honk honk.** {ctx.message.author.mention}, why? I\'m not playing something!')
             return
 
         # Now let's change the volume...
         ctx.voice_client.source.volume = volume / 100
-        await ctx.send("Honk honk! I'll honk now at {}% volume.".format(volume))
+        await ctx.send("**Honk honk!** Oke, I'll honk now at {}% volume.".format(volume))
+
+    @commands.command()
+    @commands.guild_only()
+    async def playlist(self, ctx):
+        """ Shows the playlist of the bot if present """
+
+        # Do we have a queue or are we still in a channel? If not, no playlist.
+        if ctx.guild.id not in QUEUES or ctx.voice_client is None:
+            await ctx.send('**Honk honk.** I\'m not playing anything!')
+            return
+
+        # Make an embed
+        embed = discord.Embed(
+            title='Goose\'s playlist',
+            description='These are the songs I am honking or going to honk very soon! Remember, you can add songs by using .play followed by a YouTube URL or video id.',
+            colour=self.bot.get_colour()
+        )
+
+        # Add what we are now playing.
+        embed.add_field(name='Honking...', value=QUEUES[ctx.guild.id][0]['title'], inline=False)
+
+        # And now what we are playing next, if something...
+        if len(QUEUES[ctx.guild.id]) <= 1:
+            embed.add_field(name='Honks upcoming...', value='Nothing :-(', inline=False)
+        else:
+            embed.add_field(name='Honks upcoming...',
+                            value='\n'.join([f"{i}) {QUEUES[ctx.guild.id][i]['title']}" for i in range(1, len(QUEUES[ctx.guild.id]))]),
+                            inline=False)
+
+        # Send the embed...
+        await ctx.send(embed=embed)
 
     @commands.command()
     @commands.guild_only()
     async def play(self, ctx, url: str):
-        """ Plays or queues a song from YouTube. Usage: .playsong [url] """
+        """ Plays or queues a song from YouTube, paste video id or url after .play! """
 
         # This command only works when the bot is in a voice channel...
         if ctx.voice_client is None:
-            await ctx.send('Honk honk. I\'m not in a channel!')
+            await ctx.send('**Honk honk.** I\'m not in a channel!')
             return
 
         # And also, only in the same channel...
         if ctx.message.author.voice.channel is not ctx.voice_client.channel:
-            await ctx.send('Honk honk. We\'re not in the same channel!')
+            await ctx.send(f'**Honk honk.** We\'re not in the same channel {ctx.message.author.mention}!')
             return
 
         # Declare youtube-dl options, simplified.
@@ -91,33 +126,34 @@ class Music(commands.Cog):
 
                 # Only allow if it's not longer than set amount of minutes
                 if meta['duration'] > self.bot.config.music.maxduration:
-                    await ctx.send(f'Honk honk. No, I\'m not going to honk longer than {self.bot.config.music.maxduration} seconds.')
+                    await ctx.send(f'**Honk honk.** {ctx.message.author.mention}, no, I\'m not going to honk longer than {self.bot.config.music.maxduration} seconds.')
                     return
 
-                # Are we playing already? If so, add the song to the queue.
-                if ctx.voice_client.is_playing():
+                # First let's define the object for in the queue.
+                entry = {
+                    'url': url,
+                    'title': meta['title']
+                }
 
-                    # Is there a queue already? If not, make one.
-                    if ctx.guild.id not in QUEUES:
-                        QUEUES[ctx.guild.id] = []
+                # Is there a queue already? If not, make one.
+                if ctx.guild.id not in QUEUES:
+                    QUEUES[ctx.guild.id] = []
 
-                    # Now add the song the queue.
-                    QUEUES[ctx.guild.id].append(url)
+                # Now add the song the queue.
+                QUEUES[ctx.guild.id].append(entry)
 
-                    # Inform and return.
-                    await ctx.send('Honk honk. Song added to the queue!')
+                # Now let's see if we need to start playing directly, as in, nothing is playing...
+                if not ctx.voice_client.is_playing():
+                    self.play_song(ctx)
+                    await ctx.send('**Honk honk.** Beginning to honk!')
                     return
 
-                # If code hits here, then there is no queue. Let's play.
-                self.play_song(ctx, url)
-                await ctx.send('Honk honk. Beginning to honk!')
+                # Inform and return.
+                await ctx.send(f'**Honk honk.** {ctx.message.author.mention}, I\'ve added your song to the queue!\n'
+                               f'Your song is at position #{len(QUEUES[ctx.guild.id]) - 1}, so be patient...')
 
     # Function to actually play a song.
-    def play_song(self, ctx, url=None):
-
-        # Maybe we just got .leave and aren't in a channel anymore? Queue problems...
-        if ctx.voice_client is None:
-            return
+    def play_song(self, ctx, pop=False):
 
         # Remove previous downloaded file.
         song_there = os.path.isfile(f'{ctx.guild.id}.mp3')
@@ -125,23 +161,23 @@ class Music(commands.Cog):
             if song_there:
                 os.remove(f'{ctx.guild.id}.mp3')
         except:
+            pass
+
+        # Do we have a queue or are we still in a channel? If not, end.
+        if ctx.guild.id not in QUEUES or ctx.voice_client is None:
             return
 
-        # If we don't have an url, then get one from the queue.
-        if url is None:
-
-            # Do we have a queue? If not, end.
-            if ctx.guild.id not in QUEUES:
-                return
-
-            # First check if we actually have an entry, if not stop and clear queue.
-            if len(QUEUES[ctx.guild.id]) == 0:
-                del(QUEUES[ctx.guild.id])
-                return
-
-            # Now get an url, since we do have one in the queue.
-            url = QUEUES[ctx.guild.id][0]
+        # Pop the previous song from the queue if we have to.
+        if pop:
             QUEUES[ctx.guild.id].pop(0)
+
+        # Now let's make sure we still can play something, if not delete queue from memory.
+        if len(QUEUES[ctx.guild.id]) == 0:
+            del(QUEUES[ctx.guild.id])
+            return
+
+        # Now get an url, since we do have one in the queue.
+        url = QUEUES[ctx.guild.id][0]['url']
 
         # Declare the options for youtube-dl.
         ydl_opts = {
@@ -166,7 +202,7 @@ class Music(commands.Cog):
 
         # Now let's actually start playing..
         source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(f'{ctx.guild.id}.mp3'), volume)
-        ctx.voice_client.play(source, after=lambda e: self.play_song(ctx))
-
+        ctx.voice_client.play(source, after=lambda e: self.play_song(ctx, pop=True))
+        
 def setup(bot):
     bot.add_cog(Music(bot))
