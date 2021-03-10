7dce51abddffb7877496d36dd54a46ea5722b495
Niek Esselink <niek@posteo.net>
2021-03-10 01:48:20 +0100

    Skip existing, don't crash

--- utils/migrate.py
+++ utils/migrate.py
@@ -7,14 +7,14 @@ async def go(bot):
     for guild in bot.guilds:
 
         # Add the guild itself to the database.
-        await bot.db.execute("INSERT INTO guilds (id) VALUES ($1)", guild.id)
+        await bot.db.execute("INSERT INTO guilds (id) VALUES ($1) ON CONFLICT (id) DO NOTHING", guild.id)
 
         # Get config variables and add the default of it to the database if none present.
         with open('assets/json/settings.json') as content:
             configs = json.load(content)
             for config in configs:
-                await bot.db.execute("INSERT INTO guild_settings (guild_id, key, value) VALUES ($1, $2, $3)", guild.id, config, configs[config])
+                await bot.db.execute("INSERT INTO guild_settings (guild_id, key, value) VALUES ($1, $2, $3) ON CONFLICT (guild_id, key) DO NOTHING", guild.id, config, configs[config])
 
         # Now loop through members and add them to the database as well.
         for member in guild.members:
-            await bot.db.execute("INSERT INTO guild_members (guild_id, id) VALUES ($1, $2)", guild.id, member.id)
+            await bot.db.execute("INSERT INTO guild_members (guild_id, id) VALUES ($1, $2) ON CONFLICT (guild_id, id) DO NOTHING", guild.id, member.id)
